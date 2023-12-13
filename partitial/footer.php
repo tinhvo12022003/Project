770d2404d0c9255258b27a666ca0ceade181fee7
tinhvo12022003 <94211002+tinhvo12022003@users.noreply.github.com>
2023-12-13 08:28:28 +0700

    footer web

---
+++ partitial/footer.php
@@ -0,0 +1,35 @@
+    <footer class="fixed-bottom">
+        <div class="container-fuild bg-dark text-light pt-3">
+            <div class="row justify-content-around d-flex align-items-center">
+                <div class="col-md-2">
+                    <h5 class="h5">Address</h5>
+                    <p>E St NW, Washington, DC 20037, USA</p>
+                    <p>900 23rd St NW, Washington, DC 20037, USA</p>
+                    <p> 2650 Virginia Ave NW, Washington, DC 20037, USA</p>
+                </div>
+                <div class="col-md-2">
+                    <h5 class="h5">About</h5>
+                    <img src="image/logo.jpg" alt="logo" class="rounded-circle" width="70" height="70">
+                    <p class="pt-2" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">Established in 2023
+                        global brand</p>
+                </div>
+                <div class="col-md-3">
+                    <h5 class="h5">Link</h5>
+
+                    <ul class="navbar nav d-flex">
+                        <li class="nav-item">
+                            <p><span>Facebook: www.facebook.com</span> <a href="<?php echo "index.php"; ?>"><i class="fa-brands fa-facebook fa-xl text-light pl-3"></i></a></p>
+                        </li>
+
+                        <li class="nav-item">
+                            <p><span>Instagram: www.instagram.com</span> <a href="<?php echo "index.php"; ?>"><i class="fa-brands fa-instagram text-light fa-xl pl-3"></i></a></p>
+                        </li>
+
+                        <li class="nav-item">
+                            <p><span>Twitter: www.twitter.com</span> <a href="<?php echo "index.php"; ?>"><i class="fa-brands fa-twitter text-light fa-xl pl-3"></i></a></p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
