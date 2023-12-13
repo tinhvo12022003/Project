13487709a27d3e118ee98b27759c5eac778aece5
tinhvo12022003 <94211002+tinhvo12022003@users.noreply.github.com>
2023-12-13 08:27:04 +0700

    create navbar header

---
+++ partitial/navbar.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+include_once __DIR__ . "/connect.php";
+?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="<?php
+                                        echo "index.php";
+                                        ?>" target="_blank">
+            <img src="image/logo.jpg" alt="logo" class="rounded-circle" width="50" height="50">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php
+                                                echo "index.php";
+                                                ?>">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Information
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Food</a>
+                        <a class="dropdown-item" href="#">Drink</a>
+                        <a class="dropdown-item" href="#">Snack</a>
+                        <a class="dropdown-item" href="#">Dessert</a>
+                        <div class="dropdown-divider"></div>
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a href="" class="nav-link">Order online</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+
+            <?php
+            if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
+                $query = "SELECT * FROM User WHERE username = ? AND password = ?";
+                $statment = $connect->prepare($query);
+                $statment->execute([$_SESSION['username'], $_SESSION['password']]);
+                $result = $statment->fetch(PDO::FETCH_ASSOC);
+
+                $path = "";
+
+                if(htmlspecialchars($result['img_avatar']) === ""){
+                    $path = "default-avatar.png";
+                } else {
+                    $path = htmlspecialchars($result['img_avatar']);
+                }
+                echo "
+                    <img src='../image/" . $path . "' class='rounded-circle' width='50' height='50'>
+                ";
+            } else {
+                echo "
+                    <ul class='navbar-nav '>
+                        <li class='nav-item'>
+                            <a class='nav-link my-2 my-sm-0' href='#'>Login</a>
+                            
+                        </li>
+
+                        <li class='nav-item'>
+                            
+                            <a class='nav-link my-2 my-sm-0' href='#'>Sign up</a>
+                        </li>
+                    </ul>
+                    ";
+            }
+            ?>
+
+        </div>
+    </nav>
+    </div>
